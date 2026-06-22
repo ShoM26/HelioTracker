8a3dbef6a65ca02c6020894d32430289f71806fb
MarkShort <mlshorter17@gmail.com>
2026-06-22 12:55:52 -0400

    git(): DB Migration files

---
+++ Heliotracker/Heliotracker/Migrations/20260618175629_Initial.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Heliotracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Sites",
+                columns: table => new
+                {
+                    SiteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    SiteName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SiteLatitude = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    SiteLongitude = table.Column<decimal>(type: "decimal(65,30)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sites", x => x.SiteId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Grids",
+                columns: table => new
+                {
+                    GridId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    SiteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grids", x => x.GridId);
+                    table.ForeignKey(
+                        name: "FK_Grids_Sites_SiteId",
+                        column: x => x.SiteId,
+                        principalTable: "Sites",
+                        principalColumn: "SiteId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Panels",
+                columns: table => new
+                {
+                    PanelId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    GridId = table.Column<int>(type: "int", nullable: false),
+                    LastMaintenence = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    PeakCapacityKw = table.Column<decimal>(type: "decimal(65,30)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Panels", x => x.PanelId);
+                    table.ForeignKey(
+                        name: "FK_Panels_Grids_GridId",
+                        column: x => x.GridId,
+                        principalTable: "Grids",
+                        principalColumn: "GridId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PerformanceLogs",
+                columns: table => new
+                {
+                    PerformanceLogId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PanelId = table.Column<int>(type: "int", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    EnergyGeneratedKw = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    Concerned = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PerformanceLogs", x => x.PerformanceLogId);
+                    table.ForeignKey(
+                        name: "FK_PerformanceLogs_Panels_PanelId",
+                        column: x => x.PanelId,
+                        principalTable: "Panels",
+                        principalColumn: "PanelId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grids_SiteId",
+                table: "Grids",
+                column: "SiteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Panels_GridId",
+                table: "Panels",
+                column: "GridId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PerformanceLogs_PanelId",
+                table: "PerformanceLogs",
+                column: "PanelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PerformanceLogs");
+
+            migrationBuilder.DropTable(
+                name: "Panels");
+
+            migrationBuilder.DropTable(
+                name: "Grids");
+
+            migrationBuilder.DropTable(
+                name: "Sites");
+        }
+    }
+}

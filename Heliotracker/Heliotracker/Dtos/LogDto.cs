fd6597af74a8a7b54bf008febf00b0fc7e8f19c0
MarkShort <mlshorter17@gmail.com>
2026-06-22 17:48:29 -0400

    fix(): Switched the dto and therefore the way data is stored in the database to utc. This makes it easier to convert

--- Heliotracker/Heliotracker/Dtos/LogDto.cs
+++ Heliotracker/Heliotracker/Dtos/LogDto.cs
@@ -4,6 +4,6 @@ public class LogDto
 {
     public int PerformanceLogId { get; set; }
     public int PanelId { get; set; }
-    public DateTime Date { get; set; } =  DateTime.Now;
+    public DateTime Date { get; set; } =  DateTime.UtcNow;
     public decimal EnergyGenerated {get; set;}
 }

ecdaafb1b57fe2d9a78a6d6e915d8806b0f05775
MarkShort <mlshorter17@gmail.com>
2026-06-24 11:28:45 -0400

    feat(): Adding cors, adding services, and making it so this is testable

--- Heliotracker/Heliotracker/Program.cs
+++ Heliotracker/Heliotracker/Program.cs
@@ -1,17 +1,30 @@
 using Heliotracker;
 using Microsoft.EntityFrameworkCore;
+using Heliotracker.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString =  builder.Configuration.GetConnectionString("DefaultConnection");
-
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 var version = new MySqlServerVersion(new Version(8,0,36));
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, version));
 
+builder.Services.AddScoped<IPerformanceLogService, PerformanceLogService>();
+builder.Services.AddScoped(typeof(IBaseService<>), typeof(BaseService<>));
 
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -24,7 +37,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowReactApp");
+app.UseAuthorization();
+app.MapControllers();
 
 
 app.Run();
